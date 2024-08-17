fcf0db60a47739626085be8f8ad7928d3aea8c43
🔞𝐗-𝐓𝐄𝐂𝐇🔰 <seijurolionel@gmail.com>
2024-08-17 19:58:09 +0000

    🔞𝐃𝐈𝐆𝐈𝐓𝐑𝐎𝐍𝐈𝐗-𝐓𝐄𝐂𝐇🔰

--- config.js
+++ config.js
@@ -2,20 +2,20 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // ganti info bot dibawah ini
-global.botName = "Strava V4.5"
-global.ownerName = "OwensDev"
-global.botNumber = "62xxxxxx"
-global.devNumber = ["62xxxxxxx"]
+global.botName = "X-TECH-V1.5"
+global.ownerName = "🔞𝐗-𝐓𝐄𝐂𝐇🔰"
+global.botNumber = "2250500107362","2250141523296"
+global.devNumber = ["2250500107362","2250141523296"]
 
-global.Auto_Typing = false // auto typing
+global.Auto_Typing = true // auto typing
 global.Auto_Recording = false // auto recording
 global.Auto_ReadPesan = true // auto read messages
 global.Type_Menu = "v1" // TypeMenu v1:IMAGE v2:TEXT V3:TEXT+QSTATUS
 
-global.domain = "https://example.id" // Ganti Domain Lu
+global.domain = "https://whatsapp.com/channel/0029VadaaRZK5cDOTh6sMD41" // Ganti Domain Lu
 global.key_plta = "-" // Isi Apikey Plta Lu
 global.key_pltc = "-" // Isi Apikey Pltc Lu
-global.nama_host = "OwensDev" // Ganti Nama Store Atau nama Host Lu
+global.nama_host = "X-TECH-CORP" // Ganti Nama Store Atau nama Host Lu
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
